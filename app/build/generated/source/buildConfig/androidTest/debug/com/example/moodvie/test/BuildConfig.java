5f88c2fe7d59d05137a2dd3a630af82aea3e850f
kalzeo <ixkalz@gmail.com>
2019-10-11 15:04:40 +0100

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/moodvie/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.moodvie.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.moodvie.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
