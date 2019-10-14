790dfcd1694fb129e65969f5900ed6fe809818cd
Kyle <ixkalz@gmail.com>
2019-10-14 23:37:51 +0100

    Created the UI for the login and create account activities

---
+++ app/src/main/java/com/example/moodvie/login.java
@@ -0,0 +1,16 @@
+package com.example.moodvie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
