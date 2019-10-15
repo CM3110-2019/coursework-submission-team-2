dff5325ea58d77ab0f7ee41a0c36031d2093caa5
Kyle <ixkalz@gmail.com>
2019-10-15 18:23:56 +0100

    UI for login and create account pages. Also has UI designs for landscape orientation.

---
+++ app/src/main/java/com/example/moodvie/login.java
@@ -0,0 +1,27 @@
+package com.example.moodvie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity
+{
+    protected <T extends View> T getView(int id) { return super.findViewById(id); }
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        getView(R.id.createAnAccount).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(getBaseContext(), create_account.class));
+            }
+        });
+    }
+}
