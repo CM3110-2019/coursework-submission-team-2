17b4133c5cb889520ba7fde28d29f92ac7e3a976
Kyle <ixkalz@gmail.com>
2019-10-30 02:21:03 +0000

    - Added functionality to the barcode scanner   - The name of the movie scanned is fetched using Upcitemdb API   - TMDB API used to get movie information from the Upcitemdb response

--- app/src/main/java/com/example/moodvie/BarcodeScanner.java
+++ app/src/main/java/com/example/moodvie/BarcodeScanner.java
@@ -1,21 +1,29 @@
 package com.example.moodvie;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.Result;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class BarcodeScanner extends Activity implements ZXingScannerView.ResultHandler
-{
+public class BarcodeScanner extends Activity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView mScannerView;
     functions _functions = new functions();
 
     @Override
-    public void onCreate(Bundle state)
-    {
+    public void onCreate(Bundle state) {
         super.onCreate(state);
         mScannerView = new ZXingScannerView(this); // Programmatically initialize the scanner view
         setContentView(mScannerView); // Set the scanner view as the content view
@@ -37,8 +45,126 @@ public class BarcodeScanner extends Activity implements ZXingScannerView.ResultH
     @Override
     public void handleResult(Result result)
     {
-        _functions.createMessage(getApplicationContext(), result.getText());
+        upcitemdbAPI(result.getText());
 
+    }
+
+    public void tmdbAPI(String movieName)
+    {
+        String url = getResources().getString(R.string.tmdbAPI, movieName);
+
+        // Create a Volley RequestQueue object to contain HTTP requests
+        RequestQueue mRequestQueue = Volley.newRequestQueue(this);
         mScannerView.resumeCameraPreview(this);
+
+
+        // Prepare and parse the HTTP request into a string
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>()
+        {
+            /**
+             * Get the response of the HTTP request and handle it
+             * @param response The HTTP response
+             */
+            @Override
+            public void onResponse(String response)
+            {
+                try
+                {
+                    // Set up a JSONObject to map the HTTP response since it returns JSON
+                    JSONObject rootObject = new JSONObject(response);
+                    JSONArray results = rootObject.getJSONArray("results");
+                    JSONObject zero = results.getJSONObject(0);
+
+                    String title = zero.getString("title");
+                    String overview = zero.getString("overview");
+
+                    Intent i = new Intent(BarcodeScanner.this, MoviePage.class);
+                    i.putExtra("movieTitle", title);
+                    i.putExtra("movieOverview", overview);
+
+                    // Start the activity
+                    startActivity(i);
+                }
+
+                /*
+                    If the barcode isn't found then a JSONException error is thrown, this error
+                    will be handled by finishing the activity and notifying the user that the
+                    barcode wasn't found
+                 */ catch (JSONException e) {
+                    finish();
+                    _functions.createMessage(getApplicationContext(), "Barcode Not Found In Database");
+                }
+            }
+        }, new Response.ErrorListener() {
+            /*
+                If HTTP request fails to establish a response then we need to handle the exception
+                by finishing the activity and notifying the user to check they have an active
+                Internet connection
+             */
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                finish();
+                _functions.createMessage(getApplicationContext(), "Can't Connect To API");
+            }
+        });
+
+        // Add the HTTP request to the request queue
+        mRequestQueue.add(stringRequest);
+    }
+
+    public void upcitemdbAPI(String barcode)
+    {
+        String url = getResources().getString(R.string.upcitemdbAPI, barcode);
+
+        // Create a Volley RequestQueue object to contain HTTP requests
+        RequestQueue mRequestQueue = Volley.newRequestQueue(this);
+        mScannerView.resumeCameraPreview(this);
+
+
+        // Prepare and parse the HTTP request into a string
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>()
+        {
+            /**
+             * Get the response of the HTTP request and handle it
+             * @param response The HTTP response
+             */
+            @Override
+            public void onResponse(String response)
+            {
+                try
+                {
+                    // Set up a JSONObject to map the HTTP response since it returns JSON
+                    JSONObject rootObject = new JSONObject(response);
+                    JSONArray items = rootObject.getJSONArray("items");
+                    JSONObject zero = items.getJSONObject(0);
+
+                    String movieTitle = zero.getString("title").replaceAll("\\(.*?\\) ?", "");
+                    tmdbAPI(movieTitle);
+                }
+
+                /*
+                    If the barcode isn't found then a JSONException error is thrown, this error
+                    will be handled by finishing the activity and notifying the user that the
+                    barcode wasn't found
+                 */ catch (JSONException e) {
+                    finish();
+                    _functions.createMessage(getApplicationContext(), "Barcode Not Found In Database");
+                }
+            }
+        }, new Response.ErrorListener() {
+            /*
+                If HTTP request fails to establish a response then we need to handle the exception
+                by finishing the activity and notifying the user to check they have an active
+                Internet connection
+             */
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                finish();
+                _functions.createMessage(getApplicationContext(), "Can't Connect To API");
+            }
+        });
+
+        // Add the HTTP request to the request queue
+        mRequestQueue.add(stringRequest);
     }
 }
